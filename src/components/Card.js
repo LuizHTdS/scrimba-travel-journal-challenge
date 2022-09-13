5da0871d407928e77e88ad73c9984de2b08e5d07
luiz <luizhenriquetrovao9@gmail.com>
2022-09-13 19:56:11 -0300

    renamed props

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,19 @@
 export default function Card(props) {
   return (
     <article className='card'>
-      <img src={props.item.img} alt={props.item.alt} className='card__img' />
+      <img
+        src={props.item.imgUrl}
+        alt={`picture of ${props.item.title}`}
+        className='card__img'
+      />
       <div className='card__info'>
         <div className='card__info--header'>
           <img src='../images/marker.png' alt='red map marker' />
-          <h3>{props.item.locatiopn}</h3>
+          <h3>{props.item.location}</h3>
           <a href={props.item.googleMapsUrl}>View on Google Maps</a>
         </div>
         <h1 className='card__info--title'>{props.item.title}</h1>
         <h2 className='card__info--travelDate'>
-          {' '}
           {props.item.startDate} - {props.item.endDate}
         </h2>
         <p className='card__info--desc'>{props.item.description}</p>
