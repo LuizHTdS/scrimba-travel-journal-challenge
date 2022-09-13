c20fc18571804a520a253667799169aba1639348
luiz <luizhenriquetrovao9@gmail.com>
2022-09-13 10:20:11 -0300

    added card component

---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header>
+      <img src='../images/globe.png' id='logo' alt='a small white globe' />
+      <h1>my travel journal.</h1>
+    </header>
+  );
+}
