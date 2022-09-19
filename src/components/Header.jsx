42032881a7e26bc8ec393e791fa30d70bb7a540e
luiz <luizhenriquetrovao9@gmail.com>
2022-09-19 09:50:31 -0300

    added header styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,8 @@ export default function Header() {
   return (
     <header>
       <svg
-        width='25'
-        height='25'
+        width='50'
+        height='50'
         viewBox='0 0 24 24'
         fill='none'
         xmlns='http://www.w3.org/2000/svg'
